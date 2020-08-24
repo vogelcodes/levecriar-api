0823218eb9c5ff08197bb08b61817875079847a2
Daniel Vogel <danielcvt@gmail.com>
2020-08-24 08:25:57 -0300

    cors-enabled

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,15 @@
 import 'reflect-metadata';
 import express from 'express';
+import cors from 'cors';
 import routes from './routes';
 
 import './database';
 
 const app = express();
-
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
-app.listen(3000, () => {
-  console.log('API do Enxoval LeveCriar.com.br rodando na porta 3000');
+app.listen(3333, () => {
+  console.log('API do Enxoval LeveCriar.com.br rodando na porta 3333');
 });
